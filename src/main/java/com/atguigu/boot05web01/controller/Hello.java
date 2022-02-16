bed084704994b457879be3ed6c174f4ed2ef1c83
zjf8912 <41102951+zjf8912@users.noreply.github.com>
2022-02-16 11:33:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/boot05web01/controller/Hello.java
@@ -0,0 +1,23 @@
+package com.atguigu.boot05web01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author zjf
+ * @date 2022/1/21 - 17:21
+ */
+@Controller
+public class Hello {
+    @GetMapping("/aa")
+    @ResponseBody
+    public String aa(){
+        return "Hello, Spring Boot 2!";
+    }
+
+    @GetMapping("/bb")
+    public String bb(){
+        return "bb";
+    }
+}
