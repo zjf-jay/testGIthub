aad507c4b2be576c3bffbfd192df6af32a51ce7b
zjf-jay <41102951+zjf-jay@users.noreply.github.com>
2022-02-16 14:57:50 +0800

    Update Hello.java

--- src/main/java/com/atguigu/boot05web01/controller/Hello.java
+++ src/main/java/com/atguigu/boot05web01/controller/Hello.java
@@ -17,6 +17,7 @@ public class Hello {
         System.out.println("2");
         System.out.println("2");
         System.out.println("2");
+        System.out.println("2");
         return "Hello, Spring Boot 2!";
     }
 
